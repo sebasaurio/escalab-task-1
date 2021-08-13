dd4692416042033d0b0db2334791ec5f0361f53c
Sebastian Cardoza <sebastian.b.c.d@gmail.com>
2021-08-12 21:29:19 -0400

    footer

--- src/components/footer/footer-component.jsx
+++ src/components/footer/footer-component.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
-const Footer = () => {
-    return (
-        <div>
-            
-        </div>
+import gitHub from './../../assets/github.png'
+import './footer-component.css'
+
+const Footer = () => (
+        <footer className='footer'>
+            <div className='footer-options'>
+                <Link href='#'> 
+                    <img src={gitHub} alt="Repositorio" width='16px' height='16px' />
+                    Ver repositorio
+                </Link>
+            </div>
+            <p>
+            &copy; Sebastian Cardoza Tarea 1 - Escalab
+            </p>
+        </footer>
     )
-}
 
 export default Footer
